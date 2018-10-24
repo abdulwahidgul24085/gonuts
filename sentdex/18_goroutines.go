04eff5d305588db358d69ceca96f1afdad0e598c
AbdulWahid Gul <abdulwahid.gul@mparsec.com>
2018-10-24 12:19:25 +0500

    sync wait deffer panic recover

--- sentdex/18_goroutines.go
+++ sentdex/18_goroutines.go
@@ -3,17 +3,52 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
-func say(s string) {
-	for i:= 0; i < 3; i++ {
-		fmt.Println(s)
-		time.Sleep(time.Millisecond * 100)
+// weight group
+// This waits for the go routine to complete
+var wg sync.WaitGroup
+
+func cleanup(){
+	defer wg.Done()
+	if r:= recover(); r != nil {
+		fmt.Println("Recovered in cleanup", r)
 	}
 }
 
+func say(s string) {
+	// The defer statement will be evalutated immediately, but wont run until the end, so you can, and usually will, put the defer statement early in the function
+	defer cleanup()
+	for i:= 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 100)
+		if i == 2 {
+			panic("This does not work bro")
+		}
+	}
+}
+
+func foo() {
+	defer fmt.Println("Done")
+	defer fmt.Println("Are we Done?")
+	fmt.Println("This is doing something, Like seriously")
+
+}
+
 func main() {
+	// This add's wait to the routine
+	wg.Add(1)
 	go say("abdulwahid")
+	wg.Add(1)
 	go say("gul")
-	time.Sleep(time.Second)
+	wg.Wait()
+
+	wg.Add(1)
+	go say("Boy")
+	wg.Wait()
+
+	foo()
+	// Time delay is introduced coz the code will not stop for the concurrent function to finish
+	// time.Sleep(time.Second)
 }
