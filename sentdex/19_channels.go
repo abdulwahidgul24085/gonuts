f06c1b93bb8ef9cd715ca045ca431ebdd94c0b59
AbdulWahid Gul <abdulwahid.gul@mparsec.com>
2018-10-24 12:41:17 +0500

    channel

---
+++ sentdex/19_channels.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+// the send and receive part of channel is auto blocking
+func foo(c chan int, someValue int) {
+	defer wg.Done()
+	c <- someValue * 5
+}
+
+func main() {
+	fooVal := make(chan int, 10)
+
+	// go foo(fooVal, 5)
+	// go foo(fooVal, 3)
+
+	// v1 := <- fooVal
+	// v2 := <- fooVal
+
+	// fmt.Println(v1,v2)
+
+	for i := 0; i <10; i++ {
+		wg.Add(1)
+		go foo(fooVal, i)
+	}
+	wg.Wait()
+
+	close(fooVal)
+
+	for item := range fooVal {
+		fmt.Println(item)
+	}
+}
